b73e48845dbbba1c0dd044790b79ffd7d30862a2
Viktor Grant <isahara86@gmail.com>
2022-12-06 21:18:24 +0100

    feat: add apollo reset with new token

--- src/app/@graphql/graphql.module.ts
+++ src/app/@graphql/graphql.module.ts
@@ -1,36 +1,172 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { Apollo, ApolloModule } from 'apollo-angular';
-import { InMemoryCache } from '@apollo/client/core';
+import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
+import { ApolloLink, DefaultOptions, InMemoryCache } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { setContext } from '@apollo/client/link/context';
 
 // import { environment } from 'src/environments/environment';
 // import { WebSocketLink } from '@apollo/client/link/ws';
 // const GQL_WS_ENDPOINT = environment.graphqlWSEndpoint;
 
+const uri = 'http://localhost:11300/graphql';
+
+export function createApolloWithToken(httpLink: HttpLink, token: string) {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8'
+    }
+  }));
+
+  const auth = setContext((operation, context) => {
+    // const token = localStorage.getItem('token');
+    // let token = this.auth.getCachedAccessToken();
+
+    if (token === null) {
+      return {};
+    } else {
+      return {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+    }
+  });
+
+  // // autho refresh token
+  // const auth = setContext(async(_, { headers }) => {
+  //   // Grab token if there is one in storage or hasn't expired
+  //   let token = this.auth.getCachedAccessToken();
+  //
+  //   if (!token) {
+  //     // An observable to fetch a new token
+  //     // Converted .toPromise()
+  //     await this.auth.acquireToken().toPromise();
+  //
+  //     // Set new token to the response (adal puts the new token in storage when fetched)
+  //     token = this.auth.getCachedAccessToken();
+  //   }
+  //   // Return the headers as usual
+  //   return {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   };
+  // });
+
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+
+  const defaultOptions: DefaultOptions = {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+  }
+
+  return {
+    link,
+    cache,
+    defaultOptions,
+  }
+}
+
+function createApollo(httpLink: HttpLink) {
+  const basic = setContext((operation, context) => ({
+    headers: {
+      Accept: 'charset=utf-8'
+    }
+  }));
+
+  const auth = setContext((operation, context) => {
+    const token = localStorage.getItem('token');
+    // let token = this.auth.getCachedAccessToken();
+
+    if (token === null) {
+      return {};
+    } else {
+      return {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      };
+    }
+  });
+
+  // // autho refresh token
+  // const auth = setContext(async(_, { headers }) => {
+  //   // Grab token if there is one in storage or hasn't expired
+  //   let token = this.auth.getCachedAccessToken();
+  //
+  //   if (!token) {
+  //     // An observable to fetch a new token
+  //     // Converted .toPromise()
+  //     await this.auth.acquireToken().toPromise();
+  //
+  //     // Set new token to the response (adal puts the new token in storage when fetched)
+  //     token = this.auth.getCachedAccessToken();
+  //   }
+  //   // Return the headers as usual
+  //   return {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   };
+  // });
+
+  const link = ApolloLink.from([basic, auth, httpLink.create({ uri })]);
+  const cache = new InMemoryCache();
+
+  const defaultOptions: DefaultOptions = {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+  }
+
+  return {
+    link,
+    cache,
+    defaultOptions,
+  }
+}
+
 @NgModule({
   imports: [ApolloModule, CommonModule, HttpClientModule],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    useFactory: createApollo,
+    deps: [HttpLink],
+  },],
   declarations: [],
 })
 export class GraphQLModule {
-  constructor(public apollo: Apollo) {
-    this.initApolloWithHeaders({
-      'x-hasura-admin-secret': '<your-admin-secret>',
-    });
-
-    // Use this for Token based authentication
-    //this.initApolloWithHeaders({ authorization: 'Bearer ' + token })
-  }
-
-  private initApolloWithHeaders(headers: any) {
-
-    this.apollo.create(
-      {
-        uri: 'http://localhost:11300/graphql',
-        cache: new InMemoryCache(),
-      },
-      'default'
-    );
-  }
+  // constructor(public apollo: Apollo) {
+  //   this.initApolloWithHeaders({
+  //     'x-hasura-admin-secret': '<your-admin-secret>',
+  //   });
+  //
+  //   // Use this for Token based authentication
+  //   //this.initApolloWithHeaders({ authorization: 'Bearer ' + token })
+  // }
+  //
+  // private initApolloWithHeaders(headers: any) {
+  //
+  //   this.apollo.create(
+  //     {
+  //       uri: 'http://localhost:11300/graphql',
+  //       cache: new InMemoryCache(),
+  //     },
+  //     'default'
+  //   );
+  // }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Guid } from 'guid-typescript';
 import { GraphQlService } from './@graphql/graphql.service';
 import { addWarning } from '@angular-devkit/build-angular/src/utils/webpack-diagnostics';
+import { Apollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { createApolloWithToken } from './@graphql/graphql.module';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +15,19 @@ export class AppComponent {
 
   public message: string = 'Hello!';
 
-  constructor(private readonly graphQlService: GraphQlService) {
+  constructor(private readonly graphQlService: GraphQlService, private apollo: Apollo, private readonly httpLink: HttpLink) {
     console.log(111);
   }
 
   async fetchMenu(): Promise<void> {
     console.log(await this.graphQlService.findBooks().toPromise())
+  }
+
+  async resetToken(): Promise<void> {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVmlrdG9yIiwic3ViIjozLCJpYXQiOjE2NzAzNTU5NTl9.6oD96n_chefco0l9RXQvfYx1_PJxDgIlG4zT-tNJpBA";
+    // this.apollo.client.setLink(buildApolloLink(token, this.httpLink))
+
+    await this.apollo.removeClient();
+    await this.apollo.create(createApolloWithToken(this.httpLink, token));
   }
 }
