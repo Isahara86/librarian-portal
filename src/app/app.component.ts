a0a1f345972f2b884c8188ffde798eb22adff6aa
Viktor Grant <isahara86@gmail.com>
2022-12-09 18:27:57 +0100

    feat: add full view layout & app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  appVersion = packageJson.version;
   constructor(public readonly auth: AuthService) {}
 }
