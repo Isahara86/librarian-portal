a679aba7bd7f47dbca9454cc589bc7e95bfe44a6
Viktor Grant <isahara86@gmail.com>
2022-12-09 15:03:28 +0100

    feat: password confirmation

--- src/app/pages/invite-admin/invite-admin.component.ts
+++ src/app/pages/invite-admin/invite-admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { InviteAdminGQL } from '../../@graphql/_generated';
@@ -13,11 +13,21 @@ export class InviteAdminComponent implements OnInit {
   submitted = false;
   returnUrl!: string;
   error?: string;
+  checkPasswords: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    let pass = group.get('password')?.value;
+    if (!pass) {
+      return null;
+    }
+    let confirmPass = group.get('confirmPassword')?.value
+    return pass === confirmPass ? null : {notSame: true}
+  }
 
-  loginForm = this.formBuilder.group({
+  createForm = this.formBuilder.group({
     login: ['', Validators.required],
-    password: ['', Validators.required]
-  });
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  }, {validators: this.checkPasswords});
+
 
   constructor(
     private formBuilder: FormBuilder,
@@ -35,19 +45,25 @@ export class InviteAdminComponent implements OnInit {
 
   // convenience getter for easy access to form fields
   get f() {
-    return this.loginForm.controls;
+    return this.createForm.controls;
   }
 
   async onSubmit() {
     this.submitted = true;
 
     // stop here if form is invalid
-    if (this.loginForm.invalid) {
+    if (this.createForm.invalid) {
       return;
     }
 
     this.loading = true;
-    await this.inviteAdminGQL.mutate({input: {login: this.f.login.value!, name: this.f.login.value!, password: this.f.password.value!}} )
+    await this.inviteAdminGQL.mutate({
+      input: {
+        login: this.f.login.value!,
+        name: this.f.login.value!,
+        password: this.f.password.value!
+      }
+    })
       .toPromise()
       .then(res => this.router.navigate([this.returnUrl]))
       .catch(err => {
