e473f9ec1840a877e4aacf7be5e4a4329c99137a
Viktor Grant <isahara86@gmail.com>
2022-12-11 22:31:20 +0100

    feat: book update route

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -13,7 +13,8 @@ export const appRoutes: Routes = [
     canActivate: [AdminAuthGuard]
   },
 
-  {path: 'create-book', loadComponent: () => import("./pages/create-book.component").then((m) => m.CreateBookComponent), canActivate: [AdminAuthGuard]},
+  {path: 'create-book', loadComponent: () => import("./pages/create-update-book.component").then((m) => m.CreateUpdateBookComponent), canActivate: [AdminAuthGuard]},
+  {path: 'update-book/:bookId', loadComponent: () => import("./pages/create-update-book.component").then((m) => m.CreateUpdateBookComponent), canActivate: [AdminAuthGuard]},
   {path: 'create-category',  loadComponent: () => import("./pages/create-category.component").then((m) => m.CreateCategoryComponent), canActivate: [AdminAuthGuard]},
   {path: 'invite-admin',  loadComponent: () => import("./pages/invite-admin.component").then((m) => m.InviteAdminComponent), canActivate: [AdminAuthGuard]},
   {path: 'login', loadComponent: () => import("./pages/login.component").then((m) => m.LoginComponent)},
