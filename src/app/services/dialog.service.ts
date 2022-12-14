950f95ab62e520646b8f66431eabdcc5b3509539
Viktor Grant <isahara86@gmail.com>
2022-12-15 00:47:31 +0100

    feat: loading overlay

---
+++ src/app/services/dialog.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+// import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+// import { ComponentPortal } from '@angular/cdk/portal';
+// import { SpinnerOverlayComponent } from '@app/core/spinner-overlay/spinner-overlay.component';
+import { LoadingComponent } from '../dialogs/loading.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DialogService {
+  // private overlayRef: OverlayRef = null;
+  private dialogRef?: MatDialogRef<any>;
+
+  constructor(
+    // private overlay: Overlay
+    public dialog: MatDialog,
+  ) {}
+
+  public showLoading() {
+    this.dialogRef = this.dialog.open(LoadingComponent, {
+      panelClass: 'no-background',
+      disableClose: true,
+      autoFocus: true,
+    });
+  }
+
+  // show() {
+  // // Returns an OverlayRef (which is a PortalHost)
+  //
+  // if (!this.overlayRef) {
+  //   this.overlayRef = this.overlay.create();
+  // }
+  //
+  // // Create ComponentPortal that can be attached to a PortalHost
+  // const spinnerOverlayPortal = new ComponentPortal(SpinnerOverlayComponent);
+  // const component = this.overlayRef.attach(spinnerOverlayPortal); // Attach ComponentPortal to PortalHost
+  // }
+
+  public hide() {
+    if (this.dialogRef) {
+      this.dialogRef.close();
+    }
+    // if (this.overlayRef) {
+    //   this.overlayRef?.detach();
+    // }
+  }
+}
