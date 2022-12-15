7d8605a0df26de51c779a82a96545965a6671999
Viktor Grant <isahara86@gmail.com>
2022-12-16 00:57:28 +0100

    feat: customer update

--- src/app/pages/create-update-customer.component.ts
+++ src/app/pages/create-update-customer.component.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   AbstractControl,
   FormBuilder,
+  FormControl,
   ReactiveFormsModule,
   ValidationErrors,
   ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { CreateCustomerGQL } from '../@graphql/_generated';
+import {
+  CreateCustomerGQL,
+  CustomerDetailsGQL,
+  CustomerDetailsQuery,
+  UpdateCustomerGQL,
+} from '../@graphql/_generated';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppFormComponent } from '../components/app-form.component';
 import { AppInputComponent } from '../components/app-input.component';
 import { MatButtonModule } from '@angular/material/button';
 import { isValidPhoneNumber } from 'libphonenumber-js';
 import { DialogService } from '../services/dialog.service';
+import { firstValueFrom } from 'rxjs';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-create-update-customer',
   standalone: true,
   imports: [
     CommonModule,
@@ -29,24 +35,46 @@ import { DialogService } from '../services/dialog.service';
     AppFormComponent,
     AppInputComponent,
     MatButtonModule,
+    MatInputModule,
   ],
   template: `
     <app-form [formGroup]="customerForm" (formSubmit)="onSubmit()">
-      <h2>Create Customer</h2>
+      <h2>{{ existingCustomer ? 'Update' : 'Create' }} Customer</h2>
 
       <app-input controlName="name" label="name"></app-input>
-      <app-input controlName="phone" label="phone" [prefix]="phonePrefix"></app-input>
+      <app-input
+        controlName="phone"
+        label="phone"
+        [prefix]="phonePrefix"
+        [inputType]="'number'"
+      ></app-input>
       <app-input controlName="email" label="email"></app-input>
 
+      <mat-form-field appearance="fill">
+        <mat-label>Description</mat-label>
+        <textarea
+          matInput
+          formControlName="description"
+          cdkTextareaAutosize
+          #autosize="cdkTextareaAutosize"
+          cdkAutosizeMinRows="1"
+          cdkAutosizeMaxRows="5"
+        ></textarea>
+      </mat-form-field>
+
       <mat-error *ngIf="error">{{ error }}</mat-error>
-      <button mat-flat-button color="primary" [disabled]="customerForm.invalid">Create</button>
+      <button mat-flat-button color="primary" [disabled]="customerForm.invalid">
+        {{ existingCustomer ? 'Update' : 'Create' }}
+      </button>
     </app-form>
   `,
   styles: [],
 })
-export class CreateUpdateCustomerComponent {
+export class CreateUpdateCustomerComponent implements OnInit {
   error?: string;
+  existingCustomer?: CustomerDetailsQuery['customerDetails'];
   readonly phonePrefix = '+';
+  returnUrl = '/customers-list';
   validatePhone: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
     const phone = group.value;
     if (!phone) {
@@ -57,10 +85,10 @@ export class CreateUpdateCustomerComponent {
     return isValid ? null : { notValid: true };
   };
   customerForm = this.formBuilder.group({
-    name: ['', Validators.required],
-    phone: ['', this.validatePhone],
-    email: [null],
-    description: [null],
+    name: new FormControl<string>('', { nonNullable: false, validators: Validators.required }),
+    phone: new FormControl<number | null>(null, { validators: this.validatePhone }),
+    email: new FormControl<string | null>(null),
+    description: new FormControl<string | null>(null),
   });
 
   constructor(
@@ -68,8 +96,64 @@ export class CreateUpdateCustomerComponent {
     private route: ActivatedRoute,
     private router: Router,
     private createCustomerGQL: CreateCustomerGQL,
+    private updateCustomerGQL: UpdateCustomerGQL,
+    private customerDetailsGQL: CustomerDetailsGQL,
     private dialogService: DialogService,
   ) {}
+
+  async ngOnInit(): Promise<void> {
+    const { customer, customerFindError } = await this.fetchCustomerDetails();
+    this.existingCustomer = customer;
+    this.error = customerFindError;
+
+    this.fillCustomerForm();
+  }
+
+  async fetchCustomerDetails(): Promise<{
+    customer?: CustomerDetailsQuery['customerDetails'];
+    customerFindError?: string;
+  }> {
+    const result: {
+      customer?: CustomerDetailsQuery['customerDetails'];
+      customerFindError?: string;
+    } = {
+      customer: undefined,
+      customerFindError: undefined,
+    };
+
+    const customerIdParam = this.route.snapshot.paramMap.get('customerId');
+    if (!customerIdParam) {
+      return result;
+    }
+
+    const customerIdFromRoute = Number(customerIdParam);
+
+    if (Number.isInteger(customerIdFromRoute)) {
+      const res = await firstValueFrom(this.customerDetailsGQL.fetch({ id: customerIdFromRoute }));
+      if (res?.data) {
+        result.customer = res?.data.customerDetails;
+      } else {
+        result.customerFindError = res?.error?.name;
+      }
+    } else {
+      result.customerFindError = 'Invalid customer id';
+    }
+
+    return result;
+  }
+
+  fillCustomerForm(): void {
+    if (!this.existingCustomer) {
+      return;
+    }
+
+    const { name, phone, email, description } = this.existingCustomer;
+
+    this.customerForm.controls.name.patchValue(name);
+    this.customerForm.controls.phone.patchValue(phone ? +phone : null);
+    this.customerForm.controls.email.patchValue(email || null);
+    this.customerForm.controls.description.patchValue(description || null);
+  }
 
   async onSubmit(): Promise<void> {
     const { name, phone, email, description } = this.customerForm.value;
@@ -78,18 +162,35 @@ export class CreateUpdateCustomerComponent {
       return;
     }
 
-    await this.dialogService
-      .showLoadingUntil(
-        this.createCustomerGQL.mutate({
-          input: {
-            name,
-            phone: phone ? this.phonePrefix + phone : null,
-            email,
-            description,
-          },
-        }),
-      )
-      .then(() => this.router.navigate(['/']))
-      .catch(err => (this.error = err));
+    if (this.existingCustomer) {
+      await this.dialogService
+        .showLoadingUntil(
+          this.updateCustomerGQL.mutate({
+            input: {
+              id: this.existingCustomer.id,
+              name,
+              phone: phone ? this.phonePrefix + phone : null,
+              email,
+              description,
+            },
+          }),
+        )
+        .then(() => this.router.navigate([this.returnUrl]))
+        .catch(err => (this.error = err));
+    } else {
+      await this.dialogService
+        .showLoadingUntil(
+          this.createCustomerGQL.mutate({
+            input: {
+              name,
+              phone: phone ? this.phonePrefix + phone : null,
+              email,
+              description,
+            },
+          }),
+        )
+        .then(() => this.router.navigate([this.returnUrl]))
+        .catch(err => (this.error = err));
+    }
   }
 }
