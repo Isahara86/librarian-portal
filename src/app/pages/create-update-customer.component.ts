d58b1fe42289e2af013db2561579487fa57f38a1
Viktor Grant <isahara86@gmail.com>
2022-12-15 03:04:10 +0100

    feat: create customer

--- src/app/pages/create-update-customer.component.ts
+++ src/app/pages/create-update-customer.component.ts
@@ -1,26 +1,95 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { CreateCustomerGQL } from '../@graphql/_generated';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AppFormComponent } from '../components/app-form.component';
+import { AppInputComponent } from '../components/app-input.component';
+import { MatButtonModule } from '@angular/material/button';
+import { isValidPhoneNumber } from 'libphonenumber-js';
+import { DialogService } from '../services/dialog.service';
 
 @Component({
   selector: 'app-create-update-customer',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>create-update-customer works!</p> `,
+  imports: [
+    CommonModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    AppFormComponent,
+    AppInputComponent,
+    MatButtonModule,
+  ],
+  template: `
+    <app-form [formGroup]="customerForm" (formSubmit)="onSubmit()">
+      <h2>Create Customer</h2>
+
+      <app-input controlName="name" label="name"></app-input>
+      <app-input controlName="phone" label="phone" [prefix]="phonePrefix"></app-input>
+      <app-input controlName="email" label="email"></app-input>
+
+      <mat-error *ngIf="error">{{ error }}</mat-error>
+      <button mat-flat-button color="primary" [disabled]="customerForm.invalid">Create</button>
+    </app-form>
+  `,
   styles: [],
 })
 export class CreateUpdateCustomerComponent {
-  loginForm = this.formBuilder.group({
-    login: ['', Validators.required],
-    password: ['', Validators.required],
+  error?: string;
+  readonly phonePrefix = '+';
+  validatePhone: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    const phone = group.value;
+    if (!phone) {
+      return null;
+    }
+
+    const isValid = isValidPhoneNumber(this.phonePrefix + phone);
+    return isValid ? null : { notValid: true };
+  };
+  customerForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    phone: ['', this.validatePhone],
+    email: [null],
+    description: [null],
   });
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService,
+    private createCustomerGQL: CreateCustomerGQL,
+    private dialogService: DialogService,
   ) {}
+
+  async onSubmit(): Promise<void> {
+    const { name, phone, email, description } = this.customerForm.value;
+
+    if (this.customerForm.invalid || !name) {
+      return;
+    }
+
+    await this.dialogService
+      .showLoadingUntil(
+        this.createCustomerGQL.mutate({
+          input: {
+            name,
+            phone: phone ? this.phonePrefix + phone : null,
+            email,
+            description,
+          },
+        }),
+      )
+      .then(() => this.router.navigate(['/']))
+      .catch(err => (this.error = err));
+  }
 }
